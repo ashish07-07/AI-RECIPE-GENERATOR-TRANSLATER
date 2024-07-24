b6295e4ea3d5a912296f4f3a000773dacbfc2642
B <bkashishh07@gmail.com>
2024-07-24 13:09:01 +0530

    changes

--- recipe-generator/app/page.tsx
+++ recipe-generator/app/page.tsx
@@ -1,53 +1,62 @@
 "use client";
 
-import axios from "axios";
-import { setEngine } from "crypto";
-import { NextResponse } from "next/server";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  const [input, setinput] = useState<string | null>();
-  const [ingredients, setingredients] = useState<string[]>([]);
+  const languages = [
+    { ln: "english", code: "kn" },
+    { ln: "tamil", code: "tn" },
+    { ln: "hindi", code: "hi" },
+    { ln: "telugu", code: "te" },
+    { ln: "malayalam", code: "ml" },
+    { ln: "marathi", code: "mr" },
+    { ln: "gujarati", code: "gu" },
+    { ln: "bengali", code: "bn" },
+    { ln: "tamil", code: "tn" },
+    { ln: "punjabi", code: "pa" },
+    { ln: "kannada", code: "kn" },
+    { ln: "portuguese", code: "pt-BR" },
+    { ln: "german", code: "de" },
+    { ln: "japanese", code: "ja" },
+    { ln: "korean", code: "ko" },
+    { ln: "spanish", code: "es" },
+  ];
+
+  const [language, setLanguage] = useState("");
 
   return (
     <div>
+      <h1>Search</h1>
       <input
-        className="text-fuchsia-600"
         type="text"
-        placeholder="enter the ingredients u want to use"
-        onChange={function (e) {
-          setinput(e.target.value);
-        }}
+        value={language}
+        onChange={(e) => setLanguage(e.target.value)}
       ></input>
 
       <button
-        onClick={async function submitre() {
-          try {
-            if (input) {
-              const engar = input.split(",");
-              setingredients(engar);
-              const res = await axios.post("/api/items", ingredients);
-              console.log(res.data.items);
-
-              return NextResponse.json({
-                msg: "items added to database successfully",
-                ingredients,
-              });
-            } else {
-              return NextResponse.json({
-                msg: "please enter  details ingredients",
-              });
-            }
-          } catch (e) {
-            return NextResponse.json({
-              msg: "error while adding the item",
-            });
+        onClick={() => {
+          if (language) {
+            console.log(language);
+          } else {
+            alert("wtf");
           }
         }}
       >
-        {" "}
-        CLick me baba
+        Search
       </button>
+
+      <div>
+        {languages
+          .filter((val) => {
+            const input = language.toLowerCase();
+            const in2 = val.ln.toLowerCase();
+
+            return input && in2.includes(input);
+          })
+          .map((inp) => {
+            return <div key={inp.code}>{inp.ln}</div>;
+          })}
+      </div>
     </div>
   );
 }
